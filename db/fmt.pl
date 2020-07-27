c4cab9d77c11f51ccdb3418a13562df89cb57e3c
Christopher Zell <christopher.zell@gmail.com>
2020-07-27 16:11:49 -0400

    formatter

--- db/fmt.pl
+++ db/fmt.pl
@@ -2,26 +2,24 @@
 # allows us to build out a list of insert statements 
 # for the seed file from a .csv file
 
-use strict;
-use warnings;
+use v6;
+use Time::Piece:from<Perl5>;
 
-use Time::Piece;
+my @in-lines = slurp("data1.csv").lines;
+my $out = open "transactions.sql", :w;
 
-open my $fh, "<", "data1.csv";
-open my $out, ">", "transactions.sql";
+for @in-lines -> $line {
+    $line.chomp;
+    my ($category, $date, $amount, $remarks) = $line.split(/\t/);
+    my $t = Time::Piece.strptime($date, "%m/%d/%y");
+    my $query = q:to/HERE/;
+    INSERT INTO transactions (category, date, amount, remarks) 
+                VALUES (
+                    (SELECT id FROM categories WHERE category = '%s'),
+                    '%s',
+                    %.2f,
+                    '%s');\n
+    HERE
 
-while (<$fh>) {
-    chomp;
-    my ($category, $date, $amount, $remarks) = split (/\t/);
-    my $t = Time::Piece->strptime($date, "%m/%d/%y");
-    print $out sprintf (<<HERE, $category, $t->strftime("%Y-%m-%d"), $amount, $remarks);
-        INSERT INTO transactions (category, date, amount, remarks) 
-            VALUES (
-                (SELECT id FROM categories WHERE category = '%s'),
-                '%s',
-                %.2f,
-                '%s');\n
-HERE
-    ;
+    $out.printf($query, $category, $t.strftime("%Y-%m-%d"), $amount, $remarks);
 }
-
